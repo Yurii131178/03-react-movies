b2d3c8a18f29f642446625b9ff1f1c44be1429dc
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-05-25 02:23:23 +0300

    add MG

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,27 @@
 import { Toaster } from "react-hot-toast";
 import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/Moviegrid";
+import { useState } from "react";
+import { type Movie } from "../../types/movie";
 
 export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]); // Додай типізацію масиву
+
   const handleSearch = (query: string) => {
     console.log(query);
     // Тут буде моя логіка пошуку або обробки запиту :)
+    setMovies([]); // Поки що порожній масив
   };
+
+  const handleSelectMovie = (movie: Movie) => {
+    console.log("Selected movie:", movie);
+  };
+
   return (
     <>
       <SearchBar onSubmit={handleSearch} />
       <Toaster />
+      <MovieGrid onSelect={handleSelectMovie} movies={movies} />
     </>
   );
 }
